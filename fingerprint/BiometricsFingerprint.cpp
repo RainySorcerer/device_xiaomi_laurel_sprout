Mi-A3-laurel-sprout/device-xiaomi_laurel_sprout

laurel_sprout: fingerprint: Disable fod_status on fod_ui changes
onFingerUp is not called when the finger is kept at the fod area
after it was accepted, which leads to cases where fod_status stays
enabled.

Change-Id: I95da8e12d5cac83dafdfcca286fb4145396a154b

fingerprint/BiometricsFingerprint.cpp
@@ -110,7 +110,12 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
                 continue;
             }
 
-            mDevice->extCmd(mDevice, COMMAND_NIT, readBool(fd) ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+            bool fingerDown = readBool(fd);
+            ALOGI("fod_ui status: %d", fingerDown);
+            mDevice->extCmd(mDevice, COMMAND_NIT, fingerDown ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+            if (!fingerDown) {
+                set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+            }
         }
     }).detach();
 }
@@ -469,7 +474,6 @@ Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    set(FOD_STATUS_PATH, FOD_STATUS_OFF);
     return Void();
 }
 

RainySorcerer/device_xiaomi_laurel_sprout

Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

fingerprint/BiometricsFingerprint.cpp
@@ -121,7 +121,12 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
                 continue;
             }
 
-            mDevice->extCmd(mDevice, COMMAND_NIT, readBool(fodUiFd) ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+            bool fingerDown = readBool(fodUiFd);
+            ALOGI("fod_ui status: %d", fingerDown);
+            mDevice->extCmd(mDevice, COMMAND_NIT, fingerDown ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+            if (!fingerDown) {
+                set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+            }
         }
     }).detach();
 }
@@ -487,14 +492,11 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
-    if (mHaveUdfps)
-        set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    set(FOD_STATUS_PATH, FOD_STATUS_ON);
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    if (mHaveUdfps)
-        set(FOD_STATUS_PATH, FOD_STATUS_OFF);
     return Void();
 }
 